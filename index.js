4b798afb48c68b797812d95667135ac88aed95bd
Georgiy Kuznetsov <georgijkuznecov933@gmail.com>
2023-12-27 11:43:28 +0300

    000

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+export const findMax = (numbers) => {
+    if (numbers.length === 0) {
+      return []; 
+    } else {
+      return Math.max(...numbers); 
+    }
+}
 
+export const calculateAverage = (numbers) => {
+    if (numbers.length === 0) {
+      return []; 
+    } else {
+      const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0); 
+      const average = sum / numbers.length; 
+      return average;
+    }
+}
+
+export const getEvenNumbers = (numbers) => {
+    return numbers.filter(number => number % 2 === 0); 
+}
+
+export const getIntersection = (values1, values2) => {
+    return values1.filter(value => values2.includes(value)); 
+}
+
+export const findLargestElement = (nestedArrs) => {
+    if (nestedArrs.length === 0) {
+      return []; // Возвращаем пустой массив, если входной массив пустой
+    } else {
+      let flattenedArray = nestedArrs.flat(Infinity); // Выравниваем массив, чтобы обработать все вложенные уровни
+      return Math.max(...flattenedArray); // Возвращаем наибольший элемент в массиве
+    }
+}
